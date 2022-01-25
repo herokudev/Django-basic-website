c772620b8516e703054181cf11b87def494b44bd
herokudev <ao43085@gmail.com>
2022-01-24 22:14:39 -0600

    Basic setup
    Basic setup

---
+++ playground/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculate():
+  x = 1
+  y = 2
+  return x
+
+# Create your views here.
+def say_hello(request):
+  x = calculate()
+  return render(request, 'hello.html', {'name': 'Django'})
